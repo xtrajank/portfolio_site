37f2a824ea6a8b564a3c6d4c3a5b2178a49f6e4a
xtrajank <owen.rasor8@gmal.com>
2025-03-24 18:40:08 -0600

    change directory & add themes

--- main.js
+++
@@ -1,7 +0,0 @@
-function showSection(sectionId) {
-    document.querySelectorAll("main section").forEach(section => {
-        section.style.display = "none";
-    });
-
-    document.getElementById(sectionId).style.display = "block";
-}
